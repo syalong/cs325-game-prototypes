4647cc3830c9fa24677bf33b538278d2873c834b
Shierell Yalong <syalong@masonlive.gmu.edu>
2017-04-06 05:59:56 -0400

    Trying to make my game
    It wont show up?

--- Puppy Panic/js/main.js
+++ Puppy Panic/js/main.js
@@ -1,62 +1,78 @@
-"use strict";
+	window.onload = function() {
+		// You might want to start with a template that uses GameStates:
+		//     https://github.com/photonstorm/phaser/tree/v2.6.2/resources/Project%20Templates/Basic
+		
+		// You can copy-and-paste the code from any of the examples at http://examples.phaser.io here.
+		// You will need to change the fourth parameter to "new Phaser.Game()" from
+		// 'phaser-example' to 'game', which is the id of the HTML element where we
+		// want the game to go.
+		// The assets (and code) can be found at: https://github.com/photonstorm/phaser/tree/master/examples/assets
+		// You will need to change the paths you pass to "game.load.image()" or any other
+		// loading functions to reflect where you are putting the assets.
+		// All loading functions will typically all be found inside "preload()".
+		
+		
+		"use strict";
+		
+		var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update} );
 
-function make_main_game_state( game )
-{
-    function preload() {
-        // Load an image and call it 'logo'.
-        game.load.image( 'logo', 'assets/phaser.png' );
-    }
-    
-    var bouncy;
-    
-    function create() {
-        // Create a sprite at the center of the screen using the 'logo' image.
-        bouncy = game.add.sprite( game.world.centerX, game.world.centerY, 'logo' );
-        // Anchor the sprite at its center, as opposed to its top-left corner.
-        // so it will be truly centered.
-        bouncy.anchor.setTo( 0.5, 0.5 );
-        
-        // Turn on the arcade physics engine for this sprite.
-        game.physics.enable( bouncy, Phaser.Physics.ARCADE );
-        // Make it bounce off of the world bounds.
-        bouncy.body.collideWorldBounds = true;
-        
-        // Add some text using a CSS style.
-        // Center it in X, and position its top 15 pixels from the top of the world.
-        var style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
-        var text = game.add.text( game.world.centerX, 15, "Build something amazing.", style );
-        text.anchor.setTo( 0.5, 0.0 );
-    }
-    
-    function update() {
-        // Accelerate the 'logo' sprite towards the cursor,
-        // accelerating at 500 pixels/second and moving no faster than 500 pixels/second
-        // in X or Y.
-        // This function returns the rotation angle that makes it visually match its
-        // new trajectory.
-        bouncy.rotation = game.physics.arcade.accelerateToPointer( bouncy, game.input.activePointer, 500, 500, 500 );
-    }
-    
-    return { "preload": preload, "create": create, "update": update };
-}
+		function preload() {
+			game.load.image('dog', 'assets/dog.png');
+			game.load.image('owner', 'assets/smallperson.png');
+			game.load.image('sad', 'assets/sadface.png');
+		}
+		
+		var moveme;
+		
+		var pup1;
+		var pup2;
+		var pup3;
+		var pup4;
+		var pup5;
+		var pup6;
+		
+		var control;
+		
+		function create() {
+			game.physics.StartSystem(Phaser.Physics.ARCADE);
+			
+			moveme = game.add.sprite(100, 450, 'owner');
+			game.physics.arcade.enable(moveme);
+			moveme.body.collideWorldBounds(true);
+			
+			pup1 = game.add.sprite(200, 200, 'dog');
+			pup2 = game.add.sprite(400, 200, 'dog');
+			pup3 = game.add.sprite(600, 200, 'dog');
+			pup4 = game.add.sprite(200, 200, 'dog');
+			pup5 = game.add.sprite(200, 400, 'dog');
+			pup6 = game.add.sprite(200, 600, 'dog');
+			
+			game.physics.arcade.enable([pup1, pup2, pup3, pup4, pup5, pup6]);
 
-
-window.onload = function() {
-    // You might want to start with a template that uses GameStates:
-    //     https://github.com/photonstorm/phaser/tree/v2.6.2/resources/Project%20Templates/Basic
-    
-    // You can copy-and-paste the code from any of the examples at http://examples.phaser.io here.
-    // You will need to change the fourth parameter to "new Phaser.Game()" from
-    // 'phaser-example' to 'game', which is the id of the HTML element where we
-    // want the game to go.
-    // The assets (and code) can be found at: https://github.com/photonstorm/phaser/tree/master/examples/assets
-    // You will need to change the paths you pass to "game.load.image()" or any other
-    // loading functions to reflect where you are putting the assets.
-    // All loading functions will typically all be found inside "preload()".
-    
-    var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game' );
-    
-    game.state.add( "main", make_main_game_state( game ) );
-    
-    game.state.start( "main" );
-};
+			control = game.input.keyboard.createCursorKeys();
+		}
+		
+		function attentionToDog(sprite1,sprite2)
+		{
+			
+		}
+		
+		function update() {
+			if (control.left.isDown)
+			{
+				moveme.body.velocity.x = -500;
+			}
+			if (control.right.isDown)
+			{
+				moveme.body.velocity.x = 500;
+			}
+			if (control.up.isDown)
+			{
+				moveme.body.velocity.y = -500;
+			}
+			if (control.down.isDown)
+			{
+				moveme.body.velocity.y = 500;
+			}
+		}
+	};
